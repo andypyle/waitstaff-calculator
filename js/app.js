8125d0aee0571ea86eb5555c6872aa9333c99d6d
Andrew Pyle <andy.pyle@gmail.com>
2015-05-04 21:50:01 -0700

    Added all functionality except reset button.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,64 @@
 angular.module('waitstaffCalc', [])
-	.controller('calculatorCtrl', function($scope){
-		$scope.hey = 'Hey!';
+	.controller('calculatorCtrl', function($scope, $filter){	
+
+		// Define function for cancel.
+		$scope.cancel = function(){
+			$scope.baseMeal = 0;
+			$scope.taxRate = 0;
+			$scope.tipPercent = 0;
+		}	
+
+		// Define function for reset.
+		$scope.initialize = function(){
+			$scope.meals = [];
+			$scope.chargesTotal = 0;
+			$scope.chargesSubtotal = 0;
+			$scope.chargesTip = 0;
+			$scope.myTips = 0;
+			$scope.avgTip = 0;
+			$scope.cancel();
+		}
+
+		$scope.initialize();
+
+		$scope.addMeal = function(mealBasePrice, mealTaxRate, mealTipPercent, mealSubtotal, mealTip, mealTotal){
+			$scope.chargesSubtotal = mealSubtotal;
+			$scope.chargesTip = mealTip;
+			$scope.chargesTotal = mealTotal;
+
+			$scope.myTips = 0;
+			// Function that adds to the meals object.
+			$scope.meals.push({
+				'basePrice': mealBasePrice,
+				'taxRate': mealTaxRate,
+				'tipPercent': mealTipPercent,
+				'totalPrice': mealTotal,
+				'subTotal': mealSubtotal,
+				'tips': mealTip
+			});
+
+			angular.forEach($scope.meals, function(num, i){
+				$scope.myTips += num.tips;
+				$scope.avgTip = $scope.myTips / $scope.meals.length;
+			});
+
+
+		}
+
+
+
+		$scope.calculate = function(){
+			// Function to calculate meal totals.
+			baseMeal = $scope.baseMeal;
+			taxRate = $scope.taxRate / 100;
+			tipPercent = $scope.tipPercent / 100;
+			subTotal = $scope.chargesSubtotal;
+			tipAmt = $scope.chargesTip;
+
+			subTotal = baseMeal + (baseMeal * taxRate);
+			tipAmt = subTotal * tipPercent;
+			totalPrice = subTotal + tipAmt;
+
+			$scope.addMeal(baseMeal, taxRate, tipPercent, subTotal, tipAmt, totalPrice);
+		};
 	});
